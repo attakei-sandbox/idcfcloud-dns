ef2b692fd031bc03ac60942b849e4ef8c75e3e17
Kazuya Takei <attakei@gmail.com>
2017-07-07 00:15:51 +0900

    Mod classifiers

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     package_dir={'': 'src'},
     license="MIT",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: MIT License',
     ],
 )
