fa1760fc6587eb3a5fe793a3afe8f97bd2fd6554
Kazuya Takei <attakei@gmail.com>
2017-07-06 23:06:51 +0900

    Minimum code set with tox

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+from __future__ import absolute_import, unicode_literals
+from setuptools import setup, find_packages
+import os
+import codecs
+import re
+
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+def _fetch_readme(filename):
+    readme_path = os.path.join(HERE, filename)
+    try:
+        with codecs.open(readme_path, encoding='utf8') as f:
+            readme = f.read()
+    except IOError:
+        readme = ''
+    return readme
+
+
+def _fetch_package_version(filename):
+    source_path = os.path.join(HERE, filename)
+    try:
+        with codecs.open(source_path, encoding='utf8') as f:
+            version_source = f.read()
+        version_match = re.search(
+            r"^__version__ = ['\"]([^'\"]*)['\"]", version_source, re.M)
+        if version_match:
+            return version_match.group(1)
+        raise RuntimeError("Unable to find version string.")
+    except IOError:
+        raise RuntimeError('Source is not found.')
+
+
+setup(
+    name="idcfcloud-dns",
+    version=_fetch_package_version('./idcfcloud_dns.py'),
+    url='https://github.com/attakei/idcfcloud-dns',
+    author='attakei',
+    author_email='attakei@gmail.com',
+    description='IDCF cloud DNS client',
+    long_description=_fetch_readme('README.rst'),
+    packages=find_packages(exclude=['test*']),
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
---
+++ test_dummy.py
@@ -0,0 +1,8 @@
+# -*- coding:utf8 -*-
+"""Stub test code
+"""
+import idcfcloud_dns
+
+
+def test_import():
+    assert isinstance(idcfcloud_dns.__version__, str)
