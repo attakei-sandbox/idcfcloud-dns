2fd56759b67fa0a13bde93e59de13328de0eec17
Kazuya Takei <attakei@gmail.com>
2017-07-14 01:33:13 +0900

    Implement list-command to fetch zones

--- src/idcfcloud_dns/command.py
+++ src/idcfcloud_dns/command.py
@@ -3,7 +3,9 @@
 import sys
 import argparse
 import json
+import time
 import httplib2
+from .http import build_signature
 
 
 parser = argparse.ArgumentParser()
@@ -25,12 +27,28 @@ class ListCommand(object):
         :return: status code
         :rtype: int
         """
+        expires = int(time.time()) + 60
+        signature = build_signature(
+            'GET', '/api/v1/zones',
+            self.setting.api_key, self.setting.secret_key, expires)
+        headers = {
+            'X-IDCF-APIKEY': self.setting.api_key,
+            'X-IDCF-Expires': str(expires),
+            'X-IDCF-Signature': signature,
+        }
         http = httplib2.Http()
         (resp, content) = http.request(
-            'https://dns.idcfcloud.com/api/v1/zones')
+            'https://dns.idcfcloud.com/api/v1/zones',
+            headers=headers)
         # type: ( httplib2.Response, str)
         if resp.status == 401:
             data = json.loads(content)
             sys.stderr.write(data['message'])
             return 1
+        if resp.status != 200:
+            sys.stderr.write('Error\n')
+            return 1
+        data = json.loads(content)
+        for zone in data:
+            sys.stdout.write(zone['name'] + '\n')
         return 0
