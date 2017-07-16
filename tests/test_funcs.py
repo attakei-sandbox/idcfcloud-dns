ae955f4ca835531d495ad6bd7d33b6ed6e9d2152
Kazuya Takei <attakei@gmail.com>
2017-07-17 02:42:09 +0900

    Define functest

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -4,8 +4,32 @@
 In this module, all test cases are marked 'functest' to skip unittest.
 """
 import pytest
+import os
+import sys
+import subprocess
+from mock import patch
 
 
 @pytest.mark.functest
-def test_it():
-    assert True is True
+class TestListCommand(object):
+    @patch.dict('os.environ', 
+        {   'IDCF_DNS_API_KEY': os.environ['FUNCTEST_API_KEY'],
+            'IDCF_DNS_SECRET_KEY': os.environ['FUNCTEST_SECRET_KEY'],
+        })
+    @patch.object(sys, 'argv', ['_', 'list'])
+    def test_no_zones(self, capsys):
+        from idcfcloud_dns.command import main
+        ret = main()
+        assert ret == 0
+
+    @patch.dict('os.environ', 
+        {   'IDCF_DNS_API_KEY': 'dummy_api_key',
+            'IDCF_DNS_SECRET_KEY': 'dummy_secret_key',
+        })
+    @patch.object(sys, 'argv', ['_', 'list'])
+    def test_invalid_apikey(self, capsys):
+        from idcfcloud_dns.command import main
+        ret = main()
+        assert ret == 1
+        out, err = capsys.readouterr()
+        assert 'invalid apikey' in err
